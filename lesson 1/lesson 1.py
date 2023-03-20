a9e65f8f77c7d32699004d3bdd6daaab1a2221a4
YanaShyp <shypp.71@gmail.com>
2023-03-20 13:24:23 +0200

    lessons 1 and 2

---
+++ lesson 1/lesson 1.py
@@ -0,0 +1,50 @@
+# task 1: Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної
+# взаємодії (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+
+result = first + second
+print(result)
+
+result = first - second
+print(result)
+
+result = first * second
+print(result)
+
+result = first / second
+print(result)
+
+result = first // second
+print(result)
+
+first += second
+print(first)
+
+result = first ** second
+print(result)
+
+# task 2: Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1.
+# Виведіть на екран результат кожного порівняння.
+
+third = first > second
+print(third)
+
+third = first < second
+print(third)
+
+third = first == second
+print(third)
+
+third = first != second
+print(third)
+
+# task 3: Створіть змінну - резкльтат конкатенації строк "Hello " та "world!".
+
+first_word = "Hello "
+second_word = "world!"
+
+concaten = first_word + second_word
+print(concaten)
+
