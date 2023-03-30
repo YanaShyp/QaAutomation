7f9c6d4d1b22ba1e1314f4343666c39a2ab872d8
YanaShyp <shypp.71@gmail.com>
2023-03-30 21:33:41 +0300

    Homework 4

---
+++ lesson 4/lesson 4.py
@@ -0,0 +1,34 @@
+# task 1. Є list довільних чисел, наприклад [11, 77, 4, 22, 0, 56, 5, 95, 7, 5, 87, 13, 45, 67, 44]. Напишіть код,
+# який видалить (не створить новий, а саме видалить!) з нього всі числа, які менше 21 і більше 74.
+
+my_list = [11, 77, 4, 22, 0, 56, 5, 95, 7, 5, 87, 13, 45, 67, 44]
+my_list = [range_numb for range_numb in my_list if range_numb > 21 and range_numb < 74]
+print(my_list)
+
+# task 2. Є два довільних числа які відповідають за мінімальну і максимальну ціну. Є Dict з назвами магазинів і цінами:
+# { "citos": 47.999, "BB_studio" 42.999, "mo-no": 49.999, "my-main-service": 37.245, "buy-now": 38.324,
+# "x-store": 37.166, "the-partner": 38.988, "store-123": 37.720, "roze-tka": 38.003}. Напишіть код, який знайде і виведе
+# на екран назви магазинів, ціни яких потрапляють в діапазон між мінімальною і максимальною ціною.
+# Наприклад:
+# lower_limit = 35.9
+# upper_limit = 37.339
+# > match: "x-store", "main-service"
+
+lower_limit = 37.211
+upper_limit = 40.343
+my_dict = {
+    "citos": 47.999,
+    "BB_studio": 42.999,
+    "mo-no": 49.999,
+    "my-main-service": 37.245,
+    "buy-now": 38.324,
+    "x-store": 37.166,
+    "the-partner": 38.988,
+    "store-123": 37.720,
+    "roze-tka": 38.003
+}
+
+for key, value in my_dict.items():
+    if value > lower_limit and value < upper_limit:
+        print(key)
+
