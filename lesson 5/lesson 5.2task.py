a3da5290ab7ec86fd9e86a03bc8fa0eccee1dc4c
YanaShyp <shypp.71@gmail.com>
2023-05-02 14:32:31 +0300

    5hw

--- lesson 5/lesson 5.2task.py
+++ lesson 5/lesson 5.2task.py
@@ -1,27 +1,30 @@
 import requests
 
-url = "https://script.googleusercontent.com/macros/echo?user_content_key=4QyDY0m4tvZ1dEAZFtNfGWM8_jz4FR6rkB" \
-      "sBe1YmMRBzsIRbMV7okMHwXzlWqe_auAmcMzBgLvxTP7vS3vfyegBHTBCSza4-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncG" \
-      "Qajx_ryfhECjZEnJeaAzlB7fGN7B1MHFCc8w9rFZkIFMbzYaQ6ew-oWK47SUICYEJjWO7IH20wYIzNwGlI6_8EDLjZdnSlwFGk1U" \
-      "YRsWkzviUWgNz9Jw9Md8uu&lib=MkSKfxhejqWsGOYOheri7m_3N76I4c7sb"
+url = "https://script.googleusercontent.com/macros/echo?user_content_key=0Y1M-JDzWXjjjRUelwlfgVz3yKLaAvwxWaLuwawFR" \
+      "2cG8knaMan2sYH0vynVONb6qIk-ORcb736IKDZAPKryCgJKIpa28CP-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZ" \
+      "EnCi8PQ6N_8eYeTe5LfK-X_ax0H946o-rMnLp2n2MCHyp9P3JDrH9UW-YhLbCKEJyWcgpJFBzELIxbIX5A2H1SqfNb8k7Iz8qotz9Jw9Md" \
+      "8uu&lib=MkSKfxhejqWsGOYOheri7m_3N76I4c7sb"
 
 response = requests.get(url)
-our_goods = response.json()
+our_products = response.json()
 
-goods = our_goods.get("data", [])
+products = our_products.get("goods", [])
 
 total_cost = 0
 total_without_gluten_cost = 0
 
-for good in goods:
-    good_price = good.get("price", 0)
+for product in products:
+    product_price = product.get("price", 0)
+    product_stock = product.get("remainder", 0)
+    product_cost = product_price * product_stock
 
-    if isinstance(good_price, int) or isinstance(good_price, float):
-        total_cost += good_price
+    if isinstance(product_price, (int, float)) and isinstance(product_stock, (int, float)):
+        total_cost += product_cost
 
-        if good.get("gluten") == False:
-            total_without_gluten_cost += good_price
+        if not product.get("gluten"):
+            total_without_gluten_cost += product_cost
 
 
 print(total_cost)
 print(total_without_gluten_cost)
+
