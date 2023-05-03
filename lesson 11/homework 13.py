5aef5a077a82c2d5a429b27fcacea6e081e2baa9
YanaShyp <shypp.71@gmail.com>
2023-05-03 17:04:52 +0300

    13hw

---
+++ lesson 11/homework 13.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def test_swabber_buy_button_rozetka():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(2)
+    driver.get("https://rozetka.com.ua/ua/")
+    driver.find_element(By.XPATH, "//input[@placeholder='Я шукаю...']").send_keys(f"швабра {Keys.ENTER}")
+    driver.find_element(By.XPATH,
+                        "//*[text()=' Миюча Швабра Xiaomi Deerma Spray Mop White (TB500) (З розпилювачем для "
+                        "вологого прибирання) ']").click()
+    buy_icon = driver.find_element(By.XPATH,
+                                   "//*[@class='buy-button button button--with-icon button--green button--medium "
+                                   "buy-button--tile ng-star-inserted']")
+    assert buy_icon.is_displayed()
+
+
+test_swabber_buy_button_rozetka()
