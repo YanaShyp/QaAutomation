6937132aa7f674fe306677e4c9445bf867418528
YanaShyp <shypp.71@gmail.com>
2023-05-15 17:23:01 +0300

    hw13

---
+++ lesson 12/main_page.py
@@ -0,0 +1,11 @@
+from button import Button
+from selenium.webdriver.common.by import By
+from driver import Driver
+
+
+class MainPage:
+    def __init__(self):
+        driver = Driver.get_driver()
+        self.buy_button = Button(driver.find_element(By.XPATH,
+                                   "//*[@class='buy-button button button--with-icon button--green button--medium "
+                                   "buy-button--tile ng-star-inserted']"))
