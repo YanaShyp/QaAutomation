4fbd37fbf4e9822dc26bb03f060e2c70d6372712
YanaShyp <shypp.71@gmail.com>
2023-05-31 16:28:30 +0300

    hw11

---
+++ lesson 9/hw11.py
@@ -0,0 +1,16 @@
+"""
+Function Capital accepts and returns string that cleared of everything except uppercase letters.
+In addition, the length of the string should not exceed 25 characters (anything more is simply cut off)
+"""
+def capital(string: str):
+    if isinstance(string, str) is not str:
+        print("Wrong format. Enter string")
+        return
+    only_uppercase_string = ''.join(character for character in string if character.isupper())
+
+    if len(only_uppercase_string) > 25:
+        only_uppercase_string = only_uppercase_string[:25]
+
+    print(only_uppercase_string)
+
+capital("Hi")
